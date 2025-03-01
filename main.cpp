afe46218755356d842b85c9976b9f3f5555fe5fa
Evgengnomagro42 <gnom610004@list.ru>
2025-03-01 23:16:35 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
+{
+"hello, world"
+int a = 7;
+int b = 9;
+std::cout<< "sum (a+b) " << (a+b);
+std::cout << "Hi" << std::endl
+          << "My name..." << std::endl;
+std::cout<< "\t   _Info_ " << '\n'
+std::cout<< "Name:\t \nAge:\t39" <<"\n";
+std::cout<< "Group:  318" << '\n';
+
+int a; //расстояние
+int b; //время
+std::cout << "Введите два положительных числа";
+ctd::cin >> a >> b;
+std::cout << "Test value : " << (a/b) << std::endl;
+
+std::string s;
+std::cout << "Введите текст строки: \n";
+std::cin >> s; // текст пользователя
+std::cout << s << '\n';
+
+int q, w;
+std::cout << "Введите два числа, расстояние и время";
+std::cin >> q >> w;
+std::cout << "Вы ввели данные по расстоянию и времени" << q << " " << w << std::endl;
+
+std::cout << "enter a double value: ";
+double a;
+std::cin >> a;
+std::cout << "enter one of the following : +-*/";
+char sm; //deistvie
+std::cin >> sm;
+std::cout << "enter b double value: ";
+double b;
+std::cin >> b;
+
+#include <studio.h>
+int main (){
+    printf("_%s World ! %c \n %d \t", "Hello", 'a', 10);
+    return 0;
+}
