071a326cff4c46debc8488e53c7d0f2230ed2ea7
Evgengnomagro42 <gnom610004@list.ru>
2025-10-04 07:37:11 +0300

    Create Lab_4_Task_9.cpp

---
+++ Lab_4_Task_9.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <stdio.h>
+#include <bitset>
+#include <cmath>
+#include <limits>
+#include <iomanip>
+#include <sstream>
+#include <map>
+#include <functional>
+#include "Lab_4.h"
+using namespace std;
+/*
+void print1(char (&numbers)[2][6], int count1, int count2) {
+ for(int i{}; i < count1; ++i) {
+      for(int j{}; j < count2; ++j) {
+          if (j==0||j%2==0)swap(numbers[i][j],numbers[i][j+1]);
+          std::cout << (short)numbers[i][j] << "\t";
+      }if (count2%6 == 0)cout<<'\n';
+    }
+  }
+void print2(char (&numbers)[3][4], int count1, int count2) {
+ for(int i{}; i < count1; ++i) {
+      for(int j{}; j < count2; ++j) {
+          if (j==0||j%2==0)swap(numbers[i][j],numbers[i][j+1]);
+          std::cout << (short)numbers[i][j] << "\t";
+      }if (count2%4 == 0)cout<<'\n';
+    }
+  }
+void print(char (&numbers)[], int count) {//TODO ��祬�-� qt ���� �訡��
+    //��� �।��騥 �㭪樨 ᤥ���� ⠪ �� � �����⨫��� ��ଠ�쭮
+ for(int i{}; i < count; ++i) {
+          if (i==0||i%2==0)swap(numbers[i],numbers[i+1]);
+          std::cout << (short)numbers[i] << "\t";
+    }
+  }
+void print�(char (&numbers)[1][12], int count1, int count2) {
+ for(int i{}; i < count1; ++i) {
+      for(int j{}; j < count2; ++j) {
+          if (j==0||j%2==0)swap(numbers[i][j],numbers[i][j+1]);
+          std::cout << (short)numbers[i][j] << "\t";
+      };
+    }
+  }
+int main() {
+    char nums1[2][6] {{1, 2, 3, 4, 5, 6},{11, 12, 13, 14, 15, 16}};
+    int count1 = 2;
+     int count2 = 6;
+ print1(nums1,count1, count2);
+ cout<<'\n';
+ char nums2[3][4] {{1, 2, 3, 4},{11, 12, 13, 14},{21, 22, 23, 24}};
+  int count3 = 3;
+  int count4 = 4;
+ print2(nums2,count3, count4);
+ cout<<'\n';
+    char nums[] {1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 16};
+    int count = 12;
+ print(nums, count);//TODO  ��祬�-� qt ���� �訡��
+ //��� �।��騥 �㭪樨 ᤥ���� ⠪ �� � �����⨫��� ��ଠ�쭮
+ char numss[1][12]{{1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 16}};
+ int c =1;
+ int countt = 12;
+ printe(numss, c, countt);//TODO Qt ���� - printe �� undeclared
+ //identifier, ��� ��㣨� �㭪樨 �����
+}
+}*/
