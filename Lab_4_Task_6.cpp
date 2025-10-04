540d9a92e0bcea82d1dec867a6493c61733a36cb
Evgengnomagro42 <gnom610004@list.ru>
2025-10-04 07:35:10 +0300

    Create Lab_4_Task_6.cpp

---
+++ Lab_4_Task_6.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <stdio.h>
+#include <bitset>
+#include <cmath>
+#include <limits>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <iterator>
+#include <vector>
+#include <array>
+#include <cstdlib> // ��� �㭪権 rand() � srand()
+#include <ctime> // ��� �㭪樨 time()
+#include <type_traits>
+#include <cstdint>
+#include <initializer_list>
+#include <random>
+#include <chrono>
+#include <cstring>
+#include <utility>
+#include <algorithm>
+#include <cstddef> // ��� std::nullptr_t
+#include <map>
+#include <functional>
+#include <clocale>
+#include "Lab_4.h"
+using namespace std;
+/*
+void adr_i_ykaz () {
+  cout<<"������ �� ���祭�� - 楫�� �᫮, �஡��� �᫮, ��������� �㪢�"<<'\n';
+  short m;
+  float fx;
+  char cht;
+  cin>>m>>fx>>cht;
+  cout<<"�����⥫�: �த�������஢��� �ᯮ�짮����� �����஢ & � *: \n"
+      <<"-------------------------------------------------- ------\n"
+      <<"m = "<<m<<'\n'<<"fx = "<<fx<<'\n'<<"cht = "<<cht<<'\n';
+  short *d=&m;
+  float *f=&fx;
+  char *g=&cht;
+  cout<<"�ᯮ�짮����� & ������:\n"
+      <<"-----------------------\n"
+      <<"���� m = "<<&d<<'\n'<<"���� fx = "<<&f<<'\n'<<"���� cht = "<<&g<<'\n';
+  cout<<"�ᯮ���� ������� & � *:\n"
+      <<"-----------------------------\n"
+      <<"���祭�� �� ����� m = "<<*d<<'\n'<<"���祭�� �� ����� fx = "<<*f
+      <<'\n'<<"���祭�� �� ����� cht = "<<*g<<'\n';
+  cout<<"�ᯮ���� ⮫쪮 ��६����� 㪠��⥫�:\n"
+      <<"----------------------------------\n"
+      <<"���� m = "<<d<<'\n'<<"���� fx = "<<f<<'\n'<<"���� cht = "<<g<<'\n';
+  cout<<"�ᯮ�짮����� ⮫쪮 ������ 㪠��⥫�:\n"
+      <<"----------------------------------\n"
+      <<"���祭�� �� ����� m = "<<*d<<'\n'<<"���祭�� �� ����� fx = "
+     <<*f<<'\n'<<"���祭�� �� ����� cht = "<<*g<<'\n';
+  }
+void printArrayPoYkaz(){
+    int arr[]{10,20,30,40,50};
+    int *ptr0=&arr[0];
+    int *ptr1=&arr[1];
+    int *ptr2=&arr[2];
+    int *ptr3=&arr[3];
+    int *ptr4=&arr[4];
+    cout<<"������� � �����ᮬ 0 � ���ᮬ "<<ptr0<<' '
+    <<"����� ���祭�� "<<*ptr0<<'\n';
+    cout<<"������� � �����ᮬ 1 � ���ᮬ "<<ptr1<<' '
+    <<"����� ���祭�� "<<*ptr1<<'\n';
+    cout<<"������� � �����ᮬ 2 � ���ᮬ "<<ptr2<<' '
+    <<"����� ���祭�� "<<*ptr2<<'\n';
+    cout<<"������� � �����ᮬ 3 � ���ᮬ "<<ptr3<<' '
+    <<"����� ���祭�� "<<*ptr3<<'\n';
+    cout<<"������� � �����ᮬ 4 � ���ᮬ "<<ptr4<<' '
+    <<"����� ���祭�� "<<*ptr4<<'\n';
+}*/
+/*
+int main() {
+  adr_i_ykaz ();
+  printArrayPoYkaz();
+  return 0;
+  }*/
